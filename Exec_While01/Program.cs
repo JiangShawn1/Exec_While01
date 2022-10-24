eb4cb2e6424d9b224c624a6be913092d35ad7e72
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 17:34:27 +0800

    加入專案檔案。

---
+++ Exec_While01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_While01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1+ 2 + 3 + 4 + .... + N <105
+            //請問 N 是多少 ?
+
+            int sum = 1;
+            int N = 1;
+            
+            while (sum+N+1 < 105)
+            {
+                N++;
+                sum=N+sum;            
+
+            }
+            Console.WriteLine(N);            
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//使用 while , 在畫面顯示所有可以整除 300 的整數
+         //例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+            
+            int N = 0;
+
+            while (N<=300)
+            {
+                N++;
+                if (300 % N == 0)
+                {
+                    Console.WriteLine(N);                    
+                }
+                
+                
+            }
+        }
+    }
+}
